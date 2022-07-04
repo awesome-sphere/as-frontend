cdff975257e0e0f9dcc5db1475c77daca3f0cf1e
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-07-04 16:13:08 +0700

    Add uninstalled dependency to package.json, config devserver

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  devServer: {
+    host: "0.0.0.0",
+    port: 5555,
+    https: false,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:9000",
+      },
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
 };
