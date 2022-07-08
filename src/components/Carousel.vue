943d2a5c60204594c2f789525f8cb53765ef18d6
sudo-lucifer <krittin.niu@student.mahidol.edu>
2022-07-09 01:38:47 +0700

    Add list number in carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -6,53 +6,54 @@
     show-arrows-on-hover
     hide-delimiters
   >
-    <v-carousel-item v-for="n in movie" :key="n.image_src">
-      <div>
-        <v-img
-          style="border-radius: 10%"
-          class="mx-auto mb-5"
-          max-width="300"
-          :src="n.image_src"
-        ></v-img>
-        <v-card class="mx-auto pb-4" max-width="400" elevation="1">
-          <v-card-title>
-            <v-row>
-              <v-col cols="1">
-                <v-icon color="#0f3742" class="mt-5 mr-2"
-                  >mdi-movie-open</v-icon
-                >
-              </v-col>
-              <v-col>
-                <v-list-item two-line>
-                  <v-list-item-content class="pa-0">
-                    <v-list-item-title class="text-h5">
-                      {{ n.name }}
-                    </v-list-item-title>
-                    <v-list-item-subtitle>{{
-                      n.sub_title
-                    }}</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-col>
-            </v-row>
-          </v-card-title>
-
-          <v-card-text>
-            <v-row align="center" class="mx-0">
-              <v-rating
-                :value="4.5"
-                color="amber"
-                dense
-                half-increments
-                readonly
-                size="14"
-              ></v-rating>
-
-              <div class="grey--text ms-4">4.5 (413)</div>
-            </v-row>
-          </v-card-text>
-        </v-card>
+    <v-carousel-item v-for="(n, i) in movie" :key="i" style="width: 460px">
+      <div class="number-label">
+        <span>{{ i + 1 }}</span>
       </div>
+      <v-img
+        style="border-radius: 10%"
+        class="mx-auto mb-5"
+        max-width="300"
+        :src="n.image_src"
+      ></v-img>
+      <v-card class="mx-auto pb-4" max-width="400" elevation="1">
+        <v-card-title>
+          <v-row>
+            <v-col cols="1">
+              <v-icon color="#0f3742" class="mt-5 mr-2"
+                >mdi-movie-open</v-icon
+              >
+            </v-col>
+            <v-col>
+              <v-list-item two-line>
+                <v-list-item-content class="pa-0">
+                  <v-list-item-title class="text-h5">
+                    {{ n.name }}
+                  </v-list-item-title>
+                  <v-list-item-subtitle>{{
+                    n.sub_title
+                  }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-card-title>
+
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+
+            <div class="grey--text ms-4">4.5 (413)</div>
+          </v-row>
+        </v-card-text>
+      </v-card>
     </v-carousel-item>
   </v-carousel>
 </template>
