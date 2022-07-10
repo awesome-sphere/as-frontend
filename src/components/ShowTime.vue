5dcd0168ae4b8ad21de4d5ec32ae0d7f0b13a4e8
khingxc <khingkhwanc@gmail.com>
2022-07-10 14:23:06 +0700

    fix: creating chips by loop

--- src/components/ShowTime.vue
+++ src/components/ShowTime.vue
@@ -7,9 +7,9 @@
         class="mx-auto justify-center text-center"
         color="#8a9a9f"
         >
-            <v-card-text class="black--text">
+            <v-card-title class="mx-auto text-h4 text--primary justify-center">
                 <strong>AS Cinema</strong>
-            </v-card-text>
+            </v-card-title>
         </v-card>
         <v-card border-variant="dark" class="mx-auto justify-center" width="100%" color="transparent">
         <v-chip-group
@@ -21,9 +21,10 @@
             <v-col
             md="4">
                 <v-card-title>
-                Theatre 1   
+                    Theatre 1
                 </v-card-title>
                 <v-card-text>
+                    <!-- <p class="font-weight-black" sizes="24">Theatre 1</p>  -->
                     <v-icon
                     default
                     color="blue-grey darken-4"
@@ -36,27 +37,16 @@
 
             <v-divider vertical/><v-divider vertical/>
           
-            
-            <v-col 
+            <v-col class="mx-auto" v-for="elt in theatre1" v-bind:key="elt.time"
             >
-                <v-row class="mx-auto"
-                >
-                    <v-chip class="mx-5">10:30 AM</v-chip>
 
-                    <v-chip class="mx-3">1:00 PM</v-chip>
+                <v-chip class="mx-3">{{elt.time}}</v-chip>
 
-                    <v-chip class="mx-3">3:30 PM</v-chip>
-
-                    <v-chip class="mx-3">6:00 PM</v-chip>
-
-                    <v-chip class="mx-3">8:30 PM</v-chip>
-                </v-row>
             </v-col>
+
         </v-list-item>
    
         <v-divider class="mx-4"></v-divider>
-
-        
 
         <v-list-item three-line>
             <v-col
@@ -76,21 +66,13 @@
             </v-col>
         <v-divider no-gutters vertical/><v-divider vertical/>
 
-        <v-col 
-        >
-            <v-row class="mx-auto"
-                >
-                    <v-chip class="mx-5">10:30 AM</v-chip>
+        <v-col class="mx-auto" v-for="elt in theatre2" v-bind:key="elt.time"
+            >
 
-                    <v-chip class="mx-3">1:00 PM</v-chip>
+                <v-chip class="mx-3">{{elt.time}}</v-chip>
 
-                    <v-chip class="mx-3">3:30 PM</v-chip>
-
-                    <v-chip class="mx-3">6:00 PM</v-chip>
-
-                    <v-chip class="mx-3">8:30 PM</v-chip>
-            </v-row>
         </v-col>
+
         </v-list-item>
 
         <v-divider class="mx-4"></v-divider>
@@ -113,26 +95,18 @@
             </v-col>
         <v-divider no-gutters vertical/><v-divider vertical/>
 
-        <v-col 
-        >
-            <v-row class="mx-auto"
-                >
-                    <v-chip class="mx-5">10:30 AM</v-chip>
+        <v-col class="mx-auto" v-for="elt in theatre3" v-bind:key="elt.time"
+            >
 
-                    <v-chip class="mx-3">1:00 PM</v-chip>
+            <v-chip class="mx-3">{{elt.time}}</v-chip>
 
-                    <v-chip class="mx-3">3:30 PM</v-chip>
-
-                    <v-chip class="mx-3">6:00 PM</v-chip>
-
-                    <v-chip class="mx-3">8:30 PM</v-chip>
-            </v-row>
         </v-col>
+
         </v-list-item>
 
         <v-divider class="mx-4"></v-divider>
 
-       <v-list-item three-line>
+       <v-list-item>
             <v-col
             md="4">
                 <v-card-title>
@@ -150,21 +124,13 @@
             </v-col>
         <v-divider no-gutters vertical/><v-divider vertical/>
 
-        <v-col 
-        >
-            <v-row class="mx-auto"
-                >
-                    <v-chip class="mx-5">10:30 AM</v-chip>
+        <v-col class="mx-auto" v-for="elt in theatre4" v-bind:key="elt.time"
+            >
 
-                    <v-chip class="mx-3">1:00 PM</v-chip>
+            <v-chip class="mx-3">{{elt.time}}</v-chip>
 
-                    <v-chip class="mx-3">3:30 PM</v-chip>
-
-                    <v-chip class="mx-3">6:00 PM</v-chip>
-
-                    <v-chip class="mx-3">8:30 PM</v-chip>
-            </v-row>
         </v-col>
+
         </v-list-item>
 
         <v-divider class="mx-4"></v-divider>
@@ -188,21 +154,13 @@
             </v-col>
         <v-divider no-gutters vertical/><v-divider vertical/>
 
-        <v-col 
-        >
-            <v-row class="mx-auto"
-                >
-                    <v-chip class="mx-5">10:30 AM</v-chip>
+        <v-col class="mx-auto" v-for="elt in theatre5" v-bind:key="elt.time"
+            >
 
-                    <v-chip class="mx-3">1:00 PM</v-chip>
+            <v-chip class="mx-3">{{elt.time}}</v-chip>
 
-                    <v-chip class="mx-3">3:30 PM</v-chip>
-
-                    <v-chip class="mx-3">6:00 PM</v-chip>
-
-                    <v-chip class="mx-3">8:30 PM</v-chip>
-            </v-row>
         </v-col>
+
         </v-list-item>
 
         <v-divider class="mx-4"></v-divider>
@@ -215,6 +173,70 @@
 <script>
 export default {
   name: "ShowTime",
+  data() {
+    return {
+      theatre1: [],
+      theatre2: [],
+      theatre3: [],
+      theatre4: [],
+      theatre5: [],
+    };
+  },
+
+  created() {
+    this.getTimeSlot();
+  },
+
+  methods: {
+
+    async getTimeSlot() {
+      // mock data
+      const theatre1_slot = ["10.00", "12.00", "14.00", "16.00", "18.00", "20.00"];
+      const theatre2_slot = ["09.00", "11.00", "13.00", "15.00"];
+      const theatre3_slot = ["10.30", "12.30", "14.30", "16.30", "18.30"];
+      const theatre4_slot = ["09.30", "11.30", "13.30", "15.30", "17.30", "19.30", "21.30", "23.30", "01.30"];
+      const theatre5_slot = ["08.00", "10.00", "12.00", "14.00"];
+
+      for (const elt of theatre1_slot)  {
+        this.theatre1.push({
+            theatre_id: 1,
+            time: elt 
+        });
+      }
+      for (const elt of theatre2_slot)  {
+        this.theatre2.push({
+            theatre_id: 2,
+            time: elt 
+        });
+      }
+      for (const elt of theatre3_slot)  {
+        this.theatre3.push({
+            theatre_id: 3,
+            time: elt 
+        });
+      }
+      for (const elt of theatre4_slot)  {
+        this.theatre4.push({
+            theatre_id: 4,
+            time: elt 
+        });
+      }
+      for (const elt of theatre5_slot)  {
+        this.theatre5.push({
+            theatre_id: 5,
+            time: elt 
+        });
+      }
+      
+    },
+
+    async getRandomMov(theatre_id){
+        let data_name = `theatre${theatre_id}`
+        let random_data = data_name.at(0) 
+        console.log(random_data)
+        return (random_data.lang).then()
+    }
+  },
 };
 </script>
 
