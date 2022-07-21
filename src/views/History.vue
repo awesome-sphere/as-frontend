80e7780d1be048a58a28a9eadd3d66fe67fadb75
Vanessa5331 <earth5331@gmail.com>
2022-07-22 02:24:58 +0700

    Change from about page to booking history page

---
+++ src/views/History.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-container>
+    <div class="content mb-5">
+      <h1>Booking History</h1>
+    </div>
+    <template>
+      <v-data-table
+        v-show="!loading"
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        class="elevation-1"
+      ></v-data-table>
+    </template>
+
+    <v-row align="center" justify="center">
+      <v-progress-circular
+        v-show="loading"
+        :size="70"
+        :width="7"
+        color="teal"
+        indeterminate
+        class="my-16"
+      ></v-progress-circular>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Vue from "vue";
+import store from "@/store";
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: "Title", value: "title" },
+        { text: "Location", value: "location" },
+        { text: "Theater", value: "theater_id" },
+        { text: "Seat No.", value: "seat_id" },
+        { text: "Date", value: "date" },
+        { text: "Time", value: "time" },
+        { text: "Duration (m)", value: "duration" },
+      ],
+      history: [],
+      loading: false,
+    };
+  },
+
+  created() {
+    this.getHistory();
+  },
+
+  methods: {
+    async getHistory() {
+      this.loading = true;
+      let config = {
+        headers: {
+          Authorization: store.state.webToken,
+        },
+      };
+      let result = await Vue.axios.get("/authen/get-hist", config);
+      this.history = result.data.history;
+      this.loading = false;
+    },
+  },
+};
+</script>
+
+<style>
+.content h1 {
+  vertical-align: middle;
+  font-family: "Poppins", sans-serif;
+  color: #0f3742;
+  font-size: 3em;
+  font-weight: 500;
+  line-height: 1.4em;
+}
+</style>
